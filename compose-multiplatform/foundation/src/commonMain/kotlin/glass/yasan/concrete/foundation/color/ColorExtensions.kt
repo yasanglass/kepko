904ee4c6c9ae534b81fef1a5e321826eeef775e3
Yasan Glass <yasanglass@gmail.com>
2025-12-04 00:46:13 +0100

    Change container color alpha to 0.75

--- compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/concrete/foundation/color/ColorExtensions.kt
+++ compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/concrete/foundation/color/ColorExtensions.kt
@@ -3,7 +3,7 @@ package glass.yasan.concrete.foundation.color
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 
-private const val CONTAINER_ALPHA = 0.5f
+private const val CONTAINER_ALPHA = 0.75f
 private const val LUMINANCE_THRESHOLD = 0.5f
 
 public fun Color.toContainerColor(): Color = copy(alpha = CONTAINER_ALPHA)
