2c7b21b64e2911e72eec721c135704e9810cd867
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-13 23:47:59 +0100

    Setup ContentColor handling

---
+++ compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/color/ContentColor.kt
@@ -0,0 +1,54 @@
+package glass.yasan.kepko.foundation.color
+
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
+import glass.yasan.kepko.foundation.theme.KepkoTheme
+
+@Composable
+public fun ProvideLocalContentColor(
+    color: Color,
+    content: @Composable () -> Unit,
+) {
+    CompositionLocalProvider(
+        LocalContentColor provides color.takeOrElse { KepkoTheme.colors.content },
+    ) {
+        content()
+    }
+}
+
+@Composable
+public fun contentColorFor(
+    containerColor: Color,
+): Color = when (containerColor) {
+    // Layers
+    KepkoTheme.colors.foreground -> KepkoTheme.colors.content
+    KepkoTheme.colors.midground -> KepkoTheme.colors.content
+    KepkoTheme.colors.background -> KepkoTheme.colors.content
+    // Semantic
+    KepkoTheme.colors.success -> KepkoTheme.colors.onSuccess
+    KepkoTheme.colors.information -> KepkoTheme.colors.onInformation
+    KepkoTheme.colors.caution -> KepkoTheme.colors.onCaution
+    KepkoTheme.colors.danger -> KepkoTheme.colors.onDanger
+    // Semantic Inverse
+    KepkoTheme.colors.onSuccess -> KepkoTheme.colors.success
+    KepkoTheme.colors.onInformation -> KepkoTheme.colors.information
+    KepkoTheme.colors.onCaution -> KepkoTheme.colors.caution
+    KepkoTheme.colors.onDanger -> KepkoTheme.colors.danger
+    // Content
+    KepkoTheme.colors.content -> KepkoTheme.colors.foreground
+    KepkoTheme.colors.contentSubtle -> KepkoTheme.colors.foreground
+    KepkoTheme.colors.contentDisabled -> KepkoTheme.colors.foreground
+    // Inverse Content
+    KepkoTheme.colors.inverseContent -> KepkoTheme.colors.inverseForeground
+    KepkoTheme.colors.inverseContentSubtle -> KepkoTheme.colors.inverseForeground
+    KepkoTheme.colors.inverseContentDisabled -> KepkoTheme.colors.inverseForeground
+    // Inverse Layers
+    KepkoTheme.colors.inverseForeground -> KepkoTheme.colors.inverseContent
+    KepkoTheme.colors.inverseMidground -> KepkoTheme.colors.inverseContent
+    KepkoTheme.colors.inverseBackground -> KepkoTheme.colors.inverseContent
+    // Other
+    else -> KepkoTheme.colors.content
+}
