3038f299ee94a091dbf8510f247b1c31c81b404e
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-17 22:46:58 +0100

    Mark semantic colors as experimental

--- compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/color/Colors.kt
+++ compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/color/Colors.kt
@@ -5,21 +5,30 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.graphics.Color
+import glass.yasan.kepko.foundation.annotation.ExperimentalKepkoApi
 
 @Immutable
 public class Colors internal constructor(
     isDark: Boolean,
 
+    @ExperimentalKepkoApi
     public val success: Color = ColorTokens.success,
+    @ExperimentalKepkoApi
     public val onSuccess: Color = ColorTokens.onSuccess,
 
+    @ExperimentalKepkoApi
     public val information: Color = ColorTokens.information,
+    @ExperimentalKepkoApi
     public val onInformation: Color = ColorTokens.onInformation,
 
+    @ExperimentalKepkoApi
     public val caution: Color = ColorTokens.caution,
+    @ExperimentalKepkoApi
     public val onCaution: Color = ColorTokens.onCaution,
 
+    @ExperimentalKepkoApi
     public val danger: Color = ColorTokens.danger,
+    @ExperimentalKepkoApi
     public val onDanger: Color = ColorTokens.onDanger,
 
     public val content: Color = ColorTokens.content(isDark),
