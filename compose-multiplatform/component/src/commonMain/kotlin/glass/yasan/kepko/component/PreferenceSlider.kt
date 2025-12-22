a016be65fcfe3717fb4cf0dde01d205c51274613
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-22 13:14:02 +0100

    Fix disabled slider color for PreferenceSlider

--- compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceSlider.kt
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceSlider.kt
@@ -43,6 +43,7 @@ public fun PreferenceSlider(
                     onValueChangeFinished = onValueChangeFinished,
                     valueRange = valueRange,
                     steps = steps,
+                    enabled = enabled,
                 )
                 content()
             }
