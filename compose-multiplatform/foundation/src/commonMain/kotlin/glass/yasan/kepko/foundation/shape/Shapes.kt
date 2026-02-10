7cc9b0d6c5e5b4d981f98a715172b0e8535c1626
Yasan Glass <yasanglass@gmail.com>
2026-02-10 21:52:49 +0100

    Stop marking `Shapes` as experimental

--- compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/shape/Shapes.kt
+++ compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/shape/Shapes.kt
@@ -3,9 +3,6 @@ package glass.yasan.kepko.foundation.shape
 import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
-import glass.yasan.kepko.foundation.annotation.ExperimentalKepkoApi
-
-@ExperimentalKepkoApi
 public data class Shapes(
     val extraSmall: CornerBasedShape,
     val small: CornerBasedShape,
