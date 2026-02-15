85d21394986ce46b51e243da32bcecd974498977
Yasan Glass <yasanglass@gmail.com>
2026-02-15 09:21:49 +0100

    Stop marking black and solarized styles as experimental in asPreferenceRadioGroupItem

--- component/src/commonMain/kotlin/glass/yasan/kepko/util/ThemeStyleUtils.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/util/ThemeStyleUtils.kt
@@ -1,6 +1,5 @@
 package glass.yasan.kepko.util
 
-import glass.yasan.kepko.component.PreferenceAnnotation
 import glass.yasan.kepko.component.PreferenceRadioGroupItem
 import glass.yasan.kepko.foundation.theme.ThemeStyle
 
@@ -9,13 +8,6 @@ public fun ThemeStyle.Companion.asPreferenceRadioGroupItems(): List<PreferenceRa
 
 public fun ThemeStyle.asPreferenceRadioGroupItem(): PreferenceRadioGroupItem = PreferenceRadioGroupItem(
     id = id,
-    annotation = when (this) {
-        ThemeStyle.LIGHT -> null
-        ThemeStyle.DARK -> null
-        ThemeStyle.BLACK -> PreferenceAnnotation.new
-        ThemeStyle.SOLARIZED_LIGHT -> PreferenceAnnotation.experimental
-        ThemeStyle.SOLARIZED_DARK -> PreferenceAnnotation.experimental
-    },
 ) {
     title()
 }
