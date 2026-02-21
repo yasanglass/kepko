b879008c2a47f8465499b21dcca0106f5b5b45a1
Yasan Glass <yasanglass@gmail.com>
2026-02-21 20:10:11 +0100

    Annotate `Dimensions` with `@Immutable`

--- foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/dimension/Dimensions.kt
+++ foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/dimension/Dimensions.kt
@@ -1,9 +1,11 @@
 package glass.yasan.kepko.foundation.dimension
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.unit.Dp
 
+@Immutable
 public data class Dimensions(
     val borderThickness: Dp,
     val iconSize: Dp,
