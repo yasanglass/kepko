a656efc3bba91f3eb239290d87ca9e044f972ea5
Yasan Glass <yasanglass@gmail.com>
2026-02-21 22:07:59 +0100

    Annotate `Shapes` with `@Immutable`

--- foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/shape/Shapes.kt
+++ foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/shape/Shapes.kt
@@ -1,8 +1,11 @@
 package glass.yasan.kepko.foundation.shape
 
 import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.compositionLocalOf
+
+@Immutable
 public data class Shapes(
     val extraSmall: CornerBasedShape,
     val small: CornerBasedShape,
