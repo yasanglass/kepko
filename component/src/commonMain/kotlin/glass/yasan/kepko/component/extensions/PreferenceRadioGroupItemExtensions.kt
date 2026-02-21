183c6cba5fd0c92cab0c341cbd574e58a1bc1a5e
Yasan Glass <yasanglass@gmail.com>
2026-02-22 00:40:35 +0100

    Fix JVM-only toSortedMap being used in PreferenceRadioGroupItemExtensions

--- component/src/commonMain/kotlin/glass/yasan/kepko/component/extensions/PreferenceRadioGroupItemExtensions.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/extensions/PreferenceRadioGroupItemExtensions.kt
@@ -3,4 +3,4 @@ package glass.yasan.kepko.component.extensions
 import glass.yasan.kepko.component.PreferenceRadioGroupItem
 
 public fun List<PreferenceRadioGroupItem>.groupBySegment(): Map<Int, List<PreferenceRadioGroupItem>> =
-    groupBy { it.segment }.toSortedMap()
+    groupBy { it.segment }.entries.sortedBy { it.key }.associate { it.toPair() }
