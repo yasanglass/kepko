5e11bb87d5f5edebeaa3ad5b7fb9bca4d6ca6f3b
Yasan Glass <yasanglass@gmail.com>
2026-02-22 14:31:54 +0100

    Mark PreferenceRadioGroupItem as immutable

--- component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceRadioGroupItem.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceRadioGroupItem.kt
@@ -1,11 +1,13 @@
 package glass.yasan.kepko.component
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 
 /**
  * @param segment optional segment key to group items based on
  * in [PreferenceRadioGroup] and [PreferenceRadioGroupPicker].
  * */
+@Immutable
 public data class PreferenceRadioGroupItem(
     val id: String,
     val annotation: PreferenceAnnotation? = null,
