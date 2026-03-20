cd49c144aca50d73ccd7f49f2adc9527c54a0411
Yasan Glass <yasanglass@gmail.com>
2026-03-20 19:06:29 +0100

    Revert "Set ExperimentalKepkoApi's opt-in level to warning"
    This reverts commit 067ed1fd13afa1f00e005054de8be2de31e7e599.

--- foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/annotation/ExperimentalKepkoApi.kt
+++ foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/annotation/ExperimentalKepkoApi.kt
@@ -1,7 +1,7 @@
 package glass.yasan.kepko.foundation.annotation
 
 @RequiresOptIn(
-    level = WARNING,
+    level = ERROR,
     message = "This Kepko API is experimental and is likely to change or to be removed in the future.",
 )
 @Retention(AnnotationRetention.BINARY)
