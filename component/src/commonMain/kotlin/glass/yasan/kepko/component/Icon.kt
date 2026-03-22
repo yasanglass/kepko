4fe926cf104350eff3aeef144092746f40206313
Yasan Glass <yasanglass@gmail.com>
2026-03-22 15:29:56 +0100

    Fix `size` param not being used in some of `Icon` components

--- component/src/commonMain/kotlin/glass/yasan/kepko/component/Icon.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/Icon.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import glass.yasan.kepko.foundation.theme.KepkoTheme
 import glass.yasan.kepko.resource.Icons
 import androidx.compose.material3.Icon as Material3Icon
@@ -45,7 +44,7 @@ public fun Icon(
         tint = tint,
         contentDescription = contentDescription,
         modifier = modifier
-            .size(24.dp),
+            .size(size),
     )
 }
 
@@ -63,7 +62,7 @@ public fun Icon(
         contentDescription = contentDescription,
 
         modifier = modifier
-            .size(24.dp),
+            .size(size),
     )
 }
 
