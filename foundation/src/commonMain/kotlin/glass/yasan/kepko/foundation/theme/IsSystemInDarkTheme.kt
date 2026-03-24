de49b1dc7ad7e7c3f2effb66925dfa26f4e31380
Yasan Glass <yasanglass@gmail.com>
2026-03-24 21:05:46 +0100

    Stop exposing impl details in isSystemInDarkTheme() docs

--- foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/theme/IsSystemInDarkTheme.kt
+++ foundation/src/commonMain/kotlin/glass/yasan/kepko/foundation/theme/IsSystemInDarkTheme.kt
@@ -7,9 +7,6 @@ import io.github.kdroidfilter.platformtools.darkmodedetector.isSystemInDarkMode
 
 /**
  * Replacement for `androidx.compose.foundation.isSystemInDarkTheme` which does not work reliably on desktop platforms.
- *
- * Internally uses [isSystemInDarkMode] but kept as a separate Kepko function
- * in case the underlying implementation changes.
  */
 @Composable
 public fun isSystemInDarkTheme(): Boolean = isSystemInDarkMode()
