f13f266f041925c0728da51d03b39e6414a8071f
Yasan Glass <yasanglass@gmail.com>
2026-03-28 18:37:33 +0100

    Add new icon resources

--- resource/src/commonMain/kotlin/glass/yasan/kepko/resource/Icons.kt
+++ resource/src/commonMain/kotlin/glass/yasan/kepko/resource/Icons.kt
@@ -9,11 +9,38 @@ public object Icons {
     public val add: Painter
         @Composable get() = painterResource(Res.drawable.ic_add)
 
+    public val archive: Painter
+        @Composable get() = painterResource(Res.drawable.ic_archive)
+
+    public val attachFile: Painter
+        @Composable get() = painterResource(Res.drawable.ic_attach_file)
+
+    public val attachment: Painter
+        @Composable get() = painterResource(Res.drawable.ic_attachment)
+
     public val autoAwesome: Painter
         @Composable get() = painterResource(Res.drawable.ic_auto_awesome)
 
+    public val autorenew: Painter
+        @Composable get() = painterResource(Res.drawable.ic_autorenew)
+
     public val bugReport: Painter
         @Composable get() = painterResource(Res.drawable.ic_bug_report)
+
+    public val build: Painter
+        @Composable get() = painterResource(Res.drawable.ic_build)
+
+    public val cached: Painter
+        @Composable get() = painterResource(Res.drawable.ic_cached)
+
+    public val cake: Painter
+        @Composable get() = painterResource(Res.drawable.ic_cake)
+
+    public val celebration: Painter
+        @Composable get() = painterResource(Res.drawable.ic_celebration)
+
+    public val changeCircle: Painter
+        @Composable get() = painterResource(Res.drawable.ic_change_circle)
 
     public val check: Painter
         @Composable get() = painterResource(Res.drawable.ic_check)
@@ -27,11 +54,26 @@ public object Icons {
     public val close: Painter
         @Composable get() = painterResource(Res.drawable.ic_close)
 
+    public val cloudSync: Painter
+        @Composable get() = painterResource(Res.drawable.ic_cloud_sync)
+
     public val code: Painter
         @Composable get() = painterResource(Res.drawable.ic_code)
 
+    public val contentCopy: Painter
+        @Composable get() = painterResource(Res.drawable.ic_content_copy)
+
+    public val contentCut: Painter
+        @Composable get() = painterResource(Res.drawable.ic_content_cut)
+
+    public val contentPaste: Painter
+        @Composable get() = painterResource(Res.drawable.ic_content_paste)
+
     public val delete: Painter
         @Composable get() = painterResource(Res.drawable.ic_delete)
+
+    public val directorySync: Painter
+        @Composable get() = painterResource(Res.drawable.ic_directory_sync)
 
     public val edit: Painter
         @Composable get() = painterResource(Res.drawable.ic_edit)
@@ -42,11 +84,29 @@ public object Icons {
     public val experiment: Painter
         @Composable get() = painterResource(Res.drawable.ic_experiment)
 
+    public val extension: Painter
+        @Composable get() = painterResource(Res.drawable.ic_extension)
+
+    public val extensionOff: Painter
+        @Composable get() = painterResource(Res.drawable.ic_extension_off)
+
     public val favorite: Painter
         @Composable get() = painterResource(Res.drawable.ic_favorite)
 
     public val filterBw: Painter
         @Composable get() = painterResource(Res.drawable.ic_filter_bw)
+
+    public val folderCopy: Painter
+        @Composable get() = painterResource(Res.drawable.ic_folder_copy)
+
+    public val formatQuote: Painter
+        @Composable get() = painterResource(Res.drawable.ic_format_quote)
+
+    public val history: Painter
+        @Composable get() = painterResource(Res.drawable.ic_history)
+
+    public val home: Painter
+        @Composable get() = painterResource(Res.drawable.ic_home)
 
     public val info: Painter
         @Composable get() = painterResource(Res.drawable.ic_info)
@@ -60,8 +120,20 @@ public object Icons {
     public val lockOpen: Painter
         @Composable get() = painterResource(Res.drawable.ic_lock_open)
 
+    public val memory: Painter
+        @Composable get() = painterResource(Res.drawable.ic_memory)
+
+    public val mic: Painter
+        @Composable get() = painterResource(Res.drawable.ic_mic)
+
+    public val menu: Painter
+        @Composable get() = painterResource(Res.drawable.ic_menu)
+
     public val modeNight: Painter
         @Composable get() = painterResource(Res.drawable.ic_mode_night)
+
+    public val modeOffOn: Painter
+        @Composable get() = painterResource(Res.drawable.ic_mode_off_on)
 
     public val newWindow: Painter
         @Composable get() = painterResource(Res.drawable.ic_new_window)
@@ -75,14 +147,23 @@ public object Icons {
     public val palette: Painter
         @Composable get() = painterResource(Res.drawable.ic_palette)
 
+    public val person: Painter
+        @Composable get() = painterResource(Res.drawable.ic_person)
+
     public val privacyTip: Painter
         @Composable get() = painterResource(Res.drawable.ic_privacy_tip)
 
-    public val rocket: Painter
-        @Composable get() = painterResource(Res.drawable.ic_rocket)
+    public val public_: Painter
+        @Composable get() = painterResource(Res.drawable.ic_public)
 
-    public val rocketLaunch: Painter
-        @Composable get() = painterResource(Res.drawable.ic_rocket_launch)
+    public val publicOff: Painter
+        @Composable get() = painterResource(Res.drawable.ic_public_off)
+
+    public val refresh: Painter
+        @Composable get() = painterResource(Res.drawable.ic_refresh)
+
+    public val report: Painter
+        @Composable get() = painterResource(Res.drawable.ic_report)
 
     public val resetSettings: Painter
         @Composable get() = painterResource(Res.drawable.ic_reset_settings)
@@ -90,8 +171,26 @@ public object Icons {
     public val restartAlt: Painter
         @Composable get() = painterResource(Res.drawable.ic_restart_alt)
 
+    public val rocket: Painter
+        @Composable get() = painterResource(Res.drawable.ic_rocket)
+
+    public val rocketLaunch: Painter
+        @Composable get() = painterResource(Res.drawable.ic_rocket_launch)
+
+    public val save: Painter
+        @Composable get() = painterResource(Res.drawable.ic_save)
+
+    public val search: Painter
+        @Composable get() = painterResource(Res.drawable.ic_search)
+
+    public val send: Painter
+        @Composable get() = painterResource(Res.drawable.ic_send)
+
     public val settings: Painter
         @Composable get() = painterResource(Res.drawable.ic_settings)
+
+    public val settingsBackupRestore: Painter
+        @Composable get() = painterResource(Res.drawable.ic_settings_backup_restore)
 
     public val shapeLine: Painter
         @Composable get() = painterResource(Res.drawable.ic_shape_line)
@@ -101,6 +200,36 @@ public object Icons {
 
     public val star: Painter
         @Composable get() = painterResource(Res.drawable.ic_star)
+
+    public val support: Painter
+        @Composable get() = painterResource(Res.drawable.ic_support)
+
+    public val sync: Painter
+        @Composable get() = painterResource(Res.drawable.ic_sync)
+
+    public val syncDisabled: Painter
+        @Composable get() = painterResource(Res.drawable.ic_sync_disabled)
+
+    public val syncProblem: Painter
+        @Composable get() = painterResource(Res.drawable.ic_sync_problem)
+
+    public val thumbDown: Painter
+        @Composable get() = painterResource(Res.drawable.ic_thumb_down)
+
+    public val thumbUp: Painter
+        @Composable get() = painterResource(Res.drawable.ic_thumb_up)
+
+    public val update: Painter
+        @Composable get() = painterResource(Res.drawable.ic_update)
+
+    public val verified: Painter
+        @Composable get() = painterResource(Res.drawable.ic_verified)
+
+    public val visibility: Painter
+        @Composable get() = painterResource(Res.drawable.ic_visibility)
+
+    public val visibilityOff: Painter
+        @Composable get() = painterResource(Res.drawable.ic_visibility_off)
 
     public val warning: Painter
         @Composable get() = painterResource(Res.drawable.ic_warning)
