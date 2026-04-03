c86b7e74388cdcf7191d4af8181199b6fe406122
Yasan Glass <yasanglass@gmail.com>
2026-04-03 20:38:00 +0200

    Add missing ModalBottomSheet parameters from Material3

--- component/src/commonMain/kotlin/glass/yasan/kepko/component/ModalBottomSheet.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/ModalBottomSheet.kt
@@ -1,13 +1,18 @@
 package glass.yasan.kepko.component
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheetProperties
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.ModalBottomSheet as Material3ModalBottomSheet
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -16,19 +21,31 @@ public fun ModalBottomSheet(
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
     sheetState: SheetState = rememberModalBottomSheetState(),
+    sheetMaxWidth: Dp = ModalBottomSheetDefaults.sheetMaxWidth,
+    sheetGesturesEnabled: Boolean = true,
     colors: ModalBottomSheetColors = ModalBottomSheetDefaults.colors(),
     shape: Shape = ModalBottomSheetDefaults.shape,
+    tonalElevation: Dp = 0.dp,
+    scrimColor: Color = ModalBottomSheetDefaults.scrimColor,
     dragHandle: @Composable (() -> Unit)? = null,
+    contentWindowInsets: @Composable () -> WindowInsets = { ModalBottomSheetDefaults.contentWindowInsets },
+    properties: ModalBottomSheetProperties = ModalBottomSheetProperties(),
     header: @Composable ColumnScope.() -> Unit = { HorizontalDivider(Modifier.fillMaxWidth()) },
     content: @Composable ColumnScope.() -> Unit,
 ) {
     Material3ModalBottomSheet(
         onDismissRequest = onDismissRequest,
         sheetState = sheetState,
+        sheetMaxWidth = sheetMaxWidth,
+        sheetGesturesEnabled = sheetGesturesEnabled,
         containerColor = colors.containerColor,
         contentColor = colors.contentColor,
         shape = shape,
+        tonalElevation = tonalElevation,
+        scrimColor = scrimColor,
         dragHandle = dragHandle,
+        contentWindowInsets = contentWindowInsets,
+        properties = properties,
         modifier = modifier,
     ) {
         header()
--- component/src/commonMain/kotlin/glass/yasan/kepko/component/ModalBottomSheetDefaults.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/ModalBottomSheetDefaults.kt
@@ -1,14 +1,27 @@
 package glass.yasan.kepko.component
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Dp
 import glass.yasan.kepko.foundation.theme.KepkoTheme
+import androidx.compose.material3.BottomSheetDefaults as Material3BottomSheetDefaults
 
+@OptIn(ExperimentalMaterial3Api::class)
 public object ModalBottomSheetDefaults {
 
+    public val sheetMaxWidth: Dp = Material3BottomSheetDefaults.SheetMaxWidth
+
     public val shape: Shape = RectangleShape
+
+    public val scrimColor: Color
+        @Composable get() = Material3BottomSheetDefaults.ScrimColor
+
+    public val contentWindowInsets: WindowInsets
+        @Composable get() = Material3BottomSheetDefaults.windowInsets
 
     @Composable
     public fun colors(
