a1b065a070478179e49b67594506a5fc13051b0f
Yasan Glass <yasanglass@gmail.com>
2026-04-18 15:09:55 +0200

    Implement `ButtonPrimitive` from scratch instead of relying on Material3 `Button`

--- component/src/commonMain/kotlin/glass/yasan/kepko/component/ButtonTextDefaults.kt
+++ component/src/commonMain/kotlin/glass/yasan/kepko/component/ButtonTextDefaults.kt
@@ -1,13 +1,10 @@
 package glass.yasan.kepko.component
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material3.ButtonElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import glass.yasan.kepko.foundation.theme.KepkoTheme
-import androidx.compose.material3.ButtonDefaults as Material3ButtonDefaults
 
 public object ButtonTextDefaults {
     public val ContentPadding: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
@@ -19,19 +16,4 @@ public object ButtonTextDefaults {
     public fun contentPadding(
         contentPadding: PaddingValues = ContentPadding,
     ): PaddingValues = contentPadding
-
-    @Composable
-    public fun buttonElevation(
-        defaultElevation: Dp = 0.dp,
-        pressedElevation: Dp = 0.dp,
-        focusedElevation: Dp = 0.dp,
-        hoveredElevation: Dp = 0.dp,
-        disabledElevation: Dp = 0.dp,
-    ): ButtonElevation = Material3ButtonDefaults.buttonElevation(
-        defaultElevation = defaultElevation,
-        pressedElevation = pressedElevation,
-        focusedElevation = focusedElevation,
-        hoveredElevation = hoveredElevation,
-        disabledElevation = disabledElevation,
-    )
 }
